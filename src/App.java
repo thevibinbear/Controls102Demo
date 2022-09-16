c966a4c62473ad53c8abf6e06c482e00101f32cc
Dal1t5h42k <rithviksdca@gmail.com>
2022-09-15 19:06:04 -0700

    Hi

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Car c = new Car("Joe", 1);
+        c.setSpeed(1);
+        System.out.println(c.getSpeed());
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+	private String carname = "";
+	private int speed = 0;
+	public Car(String carname, int speed) {
+        this.carname = carname;
+        this.speed = speed;
+    }
+    public static void getSound() {
+		System.out.println("vroom");
+	}
+  public String getCarname() {
+    return carname;
+  }
+  public void setCarname(String c) {
+    carname = c;
+  }
+  public int getSpeed() {
+    return speed;
+  }
+  public void setSpeed(int s) {
+    speed = s;
+  }
+  
+}
